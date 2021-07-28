e56790f6f306778df2d1c73c423d871e207afb14
CYIO <80918414+jankenmoot@users.noreply.github.com>
2021-07-28 00:13:11 -0400

    Update riggers.php

--- riggers.php
+++ riggers.php
@@ -5,28 +5,29 @@ $err = "none";
 if(($_POST["rig"] == "niggers")) {
 	$err = file_get_contents("./err/wtf.html");
 	echo $err;
-    die;
+	die;
 }
 // party pooper
 if(($_POST["rig"] == "caw")) {
 	$err = file_get_contents("./err/smartass.html");
 	echo $err;
-    die;
+	die;
 }
 if(($_POST["rig"] == "caw!")) {
 	$err = file_get_contents("./err/smartass.html");
 	echo $err;
-    die;
+	die;
 }
 // correct! good job
 if(($_POST["rig"] == "leaders")) {
 	$err = file_get_contents("./err/yes.html");
 	echo $err;
-    die;
+	die;
 }
 // if after all of this, the error code is still "none", something went wrong !!
 if($err == "none") {
-	$ers = $err = file_get_contents("./err/nope.html");
-	echo $ers;
+	$err = file_get_contents("./err/nope.html");
+	echo $err;
+	die;
 }
 ?>
