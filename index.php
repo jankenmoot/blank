4cc7db6c47223f04af6d3fec1b39562ce0a5d70c
CYIO <80918414+jankenmoot@users.noreply.github.com>
2021-07-28 00:10:24 -0400

    RACISM !!!
    why did i make this open source to begin with
    idk, make your own fill the blank videogames lmao

---
+++ index.php
@@ -0,0 +1,63 @@
+<br>
+<title>FILL THE BLANK</title>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6353HKCRWH"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6353HKCRWH');
+</script>
+ <meta content="FILL THE BLANK" property="og:title">
+  <meta content="WHAT DID DONALD SAY?" property="og:description">
+    <meta content="https://coveryourselfinoil.com/blank" property="og:url">
+    <meta content="https://coveryourselfinoil.com/oilcon.png" property="og:image">
+    <meta content="#FFA500" data-react-helmet="true" name="theme-color">
+<style>
+@font-face {
+  font-family: "Impact";
+  src: url("./impact.woff") format("woff");
+}
+body {
+	background: black;
+}
+h1 {
+	color: white;
+	font-family: helvetica;
+	line-height: 0%
+}
+p {
+	color: white;
+	font-family: helvetica;
+	line-height: 0%;
+	font-size: 20px;
+}
+@keyframes thing {
+  0% {
+    transform: rotate(5deg);
+  }
+ 50% {
+    transform: rotate(-5deg);
+  }
+ 100% {
+    transform: rotate(5deg);
+  }
+}
+.thing {
+	font-family: Impact;
+	color: black;
+	animation: thing 1s ease infinite;
+	font-weight: bold;
+	font-size: 150px;
+	-webkit-text-stroke: 2.1px white;
+}
+</style>
+<center><h1>let's play some</h1><h1 class="thing">FILL THE BLANK</h1><h1>w/video</h1><br><br><br><br><br>
+<p>listen to this video</p>
+<video width="640" height="368" controls>
+  <source src="./riggers.mp4" type="video/mp4">
+</video><p>and then type here what you think is said after the interruption at 0:06!!!</p><br>
+<form action="riggers.php" method="post">
+<input type="text" name="rig" placeholder="please use lowercase only"><br>
+<input type="submit" value="S E N D">
+</form></center>
---
+++ riggers.php
@@ -0,0 +1,32 @@
+<?php
+// sets error code to none
+$err = "none";
+// sets error code to slur(tm)
+if(($_POST["rig"] == "niggers")) {
+	$err = file_get_contents("./err/wtf.html");
+	echo $err;
+    die;
+}
+// party pooper
+if(($_POST["rig"] == "caw")) {
+	$err = file_get_contents("./err/smartass.html");
+	echo $err;
+    die;
+}
+if(($_POST["rig"] == "caw!")) {
+	$err = file_get_contents("./err/smartass.html");
+	echo $err;
+    die;
+}
+// correct! good job
+if(($_POST["rig"] == "leaders")) {
+	$err = file_get_contents("./err/yes.html");
+	echo $err;
+    die;
+}
+// if after all of this, the error code is still "none", something went wrong !!
+if($err == "none") {
+	$ers = $err = file_get_contents("./err/nope.html");
+	echo $ers;
+}
+?>
